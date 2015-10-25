bfddc592e7923c86cb0547665f52a1777a0e9734
edloidas <edloidas@gmail.com>
2015-10-25 16:02:46 +0300

    Add basic test config

---
+++ test/default.js
@@ -0,0 +1,15 @@
+const should = require( 'should' );
+
+const babel = require( 'babel' );
+
+// Just a dummy test for now
+describe( 'default', () => {
+	it( 'should be initialized without errors', ( done ) => {
+		babel.should.not.be.empty();
+		done();
+	});
+
+	it( 'should run without errors', ( done ) => {
+		done();
+	});
+});
